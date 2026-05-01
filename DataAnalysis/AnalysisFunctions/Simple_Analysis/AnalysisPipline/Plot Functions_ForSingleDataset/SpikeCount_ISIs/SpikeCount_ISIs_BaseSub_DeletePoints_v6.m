5498c189501cb8a5327c3dfdc57ea79a87852849
Xiaofan Jiang <Xiaofan.Jiang@monash.edu>
2026-05-01 17:09:57 +1000

    ISI Analysis Code Update

--- DataAnalysis/AnalysisFunctions/Simple_Analysis/AnalysisPipline/Plot Functions_ForSingleDataset/SpikeCount_ISIs/SpikeCount_ISIs_BaseSub_DeletePoints_v6.m
+++ DataAnalysis/AnalysisFunctions/Simple_Analysis/AnalysisPipline/Plot Functions_ForSingleDataset/SpikeCount_ISIs/SpikeCount_ISIs_BaseSub_DeletePoints_v6.m
@@ -10,7 +10,7 @@ addpath(genpath('/Volumes/MACData/Data/Data_Xia/AnalysisFunctions'));
 
 %% ================= USER SETTINGS ============================
 % Single dataset folder containing both Sim (PTD=0) and Seq
-data_folder = '/Volumes/MACData/Data/Data_Xia/DX020/Xia_ISI_10uA_SimSeq3'; 
+data_folder = '/Volumes/MACData/Data/Data_Xia/DX024/Xia_ISI_10uA_SimSeq1'; 
 Electrode_Type = 2; % 0:single shank rigid; 1:single shank flex; 2:four shank flex
 
 % Select which ISIs (PTDs) you want to analyze. 
@@ -293,7 +293,7 @@ end
 %% ============================================================
 %   6. SAVE RESULTS
 % ============================================================
-save_dir = '/Volumes/MACData/Data/Data_Xia/Analyzed_Results/Multi_ISIs_SpikeCount/DX020/';
+save_dir = '/Volumes/MACData/Data/Data_Xia/Analyzed_Results/Multi_ISIs_SpikeCount/DX024/';
 if ~exist(save_dir, 'dir'), mkdir(save_dir); end
 parts = split(data_folder, filesep); exp_id = parts{end};
 
@@ -310,7 +310,7 @@ else
     isi_str = strjoin(string(target_ISIs), '_');
 end
 
-out_filename = fullfile(save_dir, ['Result_SpikeCount_FixWin_DX020_' char(amp_str) 'uA_' exp_id '.mat']);
+out_filename = fullfile(save_dir, ['Result_SpikeCount_FixWin_DX024_' char(amp_str) 'uA_' exp_id '.mat']);
 
 ResultFR = struct();
 ResultFR.Metadata.Created = datestr(now);
--- DataAnalysis/AnalysisFunctions/Simple_Analysis/AnalysisPipline/Response Analysis/RespondingChn_identify_SimSeq_MultiISIs_v1.m
+++ DataAnalysis/AnalysisFunctions/Simple_Analysis/AnalysisPipline/Response Analysis/RespondingChn_identify_SimSeq_MultiISIs_v1.m
@@ -6,7 +6,7 @@ clear;
 addpath(genpath('/Volumes/MACData/Data/Data_Xia/AnalysisFunctions'));
 
 %% ================= USER INPUT =================
-data_folder = '/Volumes/MACData/Data/Data_Xia/DX020/Xia_ISI_10uA_SimSeq3';
+data_folder = '/Volumes/MACData/Data/Data_Xia/DX024/Xia_ISI_10uA_SimSeq1';
 Electrode_Type = 2;    % 0 = rigid, 1 = single-shank flex, 2 = four-shank flex
 
 % ---- Choose detection mode ----
